eb0f83ddc92fdd0d8190cc659bb68e3036813b37
lorcancreedon <l.creedon3@nuigalway.ie>
2020-11-19 00:12:59 +0000

    Constructors, arraylist adding and Getter/setters to Module & Course

--- StudentRegistration/src/main/java/com/mycompany/studentregistration/Course.java
+++ StudentRegistration/src/main/java/com/mycompany/studentregistration/Course.java
@@ -4,11 +4,80 @@
  * and open the template in the editor.
  */
 package com.mycompany.studentregistration;
+import java.util.ArrayList;
+import org.joda.time.*;
+
+
 
 /**
  *
  * @author lorca
  */
 public class Course {
+    protected String cName;
+    protected ArrayList<Module> modules;
+    protected ArrayList<Student> students;
+    protected LocalDate startDate;
+    protected LocalDate endDate;
+
+    public Course(String cName, LocalDate startDate, LocalDate endDate){
+        this.cName = cName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.modules = new ArrayList<>();
+        this.students = new ArrayList<>();
+    }
     
+    //Accessor Methods
+    public String getCName(){
+        return cName;
+    }
+    
+    public ArrayList<Module> getModules(){
+        return modules;
+    }
+    
+    public ArrayList<Student> getStudents(){
+        return students;
+    }
+    
+    public LocalDate getStartDate(){
+        return startDate;
+    }
+    
+    public LocalDate getEndDate(){
+        return endDate;
+    }
+    
+    //mutator methods
+    public void setCName(String cName){
+        this.cName = cName;
+    }
+    
+    public void setModules(ArrayList<Module> modules){
+        this.modules = modules;
+    }
+    
+    public void setStudents(ArrayList<Student> students){
+        this.students = students;
+    }
+    
+    public void addModules(Module mod){
+        ArrayList<Module> addMod = modules;
+        addMod.add(mod);
+        setModules(addMod);
+    }
+    
+    public void addStudents(Student stu){
+        ArrayList<Student> addStu = students;
+        addStu.add(stu);
+        setStudents(addStu);
+    }
+    
+    @Override
+    public String toString(){
+        String s = "Course:"+cName;
+        return s;
+    }
 }
+
--- StudentRegistration/src/main/java/com/mycompany/studentregistration/Module.java
+++ StudentRegistration/src/main/java/com/mycompany/studentregistration/Module.java
@@ -4,12 +4,69 @@
  * and open the template in the editor.
  */
 package com.mycompany.studentregistration;
+import java.util.ArrayList;
 
 /**
  *
  * @author lorca
  */
 public class Module {
-    protected 
+    protected String mName;
+    protected String mID;
+    protected ArrayList<Student> students;
+    protected ArrayList<Course> courses;
     
+    public Module(String mName, String mID){
+        this.mName = mName;
+        this.mID = mID;
+        this.students = new ArrayList<>();
+        this.courses = new ArrayList<>();
+    }
+    
+    //accessor methods
+    public String getMName(){
+        return mName;
+    }
+    
+    public String getMId(){
+        return mID;
+    }
+    
+    public ArrayList<Student> getStudents(){
+        return students;
+    }
+    
+    public ArrayList<Course> getCourses(){
+        return courses;
+    }
+    
+    //mutator methods
+    public void setMName(String mName){
+        this.mName = mName;
+    }
+    
+    public void setMId(String mID){
+        this.mID = mID;
+    }
+    
+    public void setStudents(ArrayList<Student> students){
+        this.students = students;
+    }
+    
+    public void setCourses(ArrayList<Course> courses){
+        this.courses = courses;
+    }
+    
+    //adding students and courses to Module function
+    public void addStudents(Student stu){
+        ArrayList<Student> addStu = students;
+        addStu.add(stu);
+        setStudents(addStu);
+    }
+    
+    public void addCourses(Course cour){
+        ArrayList<Course> addCour = courses;
+        addCour.add(cour);
+        setCourses(addCour);
+    }
 }
